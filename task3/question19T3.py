c7d890722bf3edf30c2914c8a18222c3dac80268
unknown <chalida.jua@gmail.com>
2020-10-15 13:43:09 -0400

    Add assignment

--- task3/question19T3.py
+++ task3/question19T3.py
@@ -1,6 +1,6 @@
+from itertools import product, permutations
 x=[1,2,3,4,5,6,7,8,9,-1]
-c=np.array(x)
-for i in c :
+target_number = 8
 
-
-
+solutions = [pair for pair in permutations(x, 2) if sum(pair) == 8]
+print('The pair of numbers whose sum equals to 8 :', solutions)
---
+++ task3/question9T3.py
@@ -0,0 +1,6 @@
+d = dict()
+
+for x in range(1, 6):
+    d[x]=x*x
+
+print(d) 
